0ee3408932be6d652ce9efedc16b84ad830b639b
Eduardo Maya <ediij1994@gmail.com>
2023-12-17 17:38:48 -0500

    UPDATE tarea1 finalizada

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 
-console.log(galleryItems);
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let gallery = document.querySelector(".gallery");
+
+const newImages = [...galleryItems]
+  .map(
+    (galleryItem) =>
+      `
+<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" title="${galleryItem.description}"/>
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImages);
+
+gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  gallery.options.CaptionDelay = 250;
+});
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // Some usefull information
+});
