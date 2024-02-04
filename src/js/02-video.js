f260cf3cf0b0e6f820245939c157176d1e34f79b
Eduardo Maya <ediij1994@gmail.com>
2024-02-03 20:11:01 -0500

    UPDATE tarea 2 vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+
+const vimeoplayer = new Player(iframe);
+
+let currentTime = localStorage.getItem('videoplayer-current-time')
+  ? localStorage.getItem('videoplayer-current-time')
+  : 0;
+
+vimeoplayer.setCurrentTime(currentTime);
+
+vimeoplayer.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+vimeoplayer.on('pause', ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+});
